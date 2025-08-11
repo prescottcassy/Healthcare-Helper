d3cc69316470b5645fcacd6ac4fa66b15bc71e40
Cassy Cormier <93752385+prescottcassy@users.noreply.github.com>
2025-08-11 17:03:39 +0000

    uupdate uvicorn port

--- start_backend.sh
+++ start_backend.sh
@@ -2,4 +2,4 @@
 # This script starts the FastAPI backend using Uvicorn from the project root
 
 export $(grep -v '^#' .env | xargs)  # Load environment variables from .env
-uvicorn backend_folder.main:app --host 0.0.0.0 --port 8000
+uvicorn backend_folder.main:app --host 0.0.0.0 --port 10000
