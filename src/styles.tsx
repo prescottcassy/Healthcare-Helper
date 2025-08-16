14f1ed2140ff21fd506bd28e8f17f96137cb4415
Cassy Prescott <prescottcassy@gmail.com>
2025-08-16 13:55:24 -0500

    Clean up manual docs folder

---
+++ src/styles.tsx
@@ -0,0 +1,11 @@
+import './css/main.css'; // Global styles
+import { Container } from './styles'; // Styled component
+
+function App() {
+  return (
+    <Container>
+      <h1>Healthcare AI Assistant</h1>
+      {/* other components */}
+    </Container>
+  );
+}
