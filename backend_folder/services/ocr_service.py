4f05fe8cba664c1032e41bb4d5f5bd985d58156f
Cassy Prescott <prescottcassy@gmail.com>
2025-08-16 16:04:29 -0500

    Update ocr_service

--- backend_folder/services/ocr_service.py
+++ backend_folder/services/ocr_service.py
@@ -1,16 +1,12 @@
-import pytesseract
-from PIL import Image
-import io
+import tempfile
+from backend_folder.insurance_analyzer import insurance_ocr
 
 def extract_insurance_info(file_bytes: bytes) -> dict:
-    """Extract insurance info from uploaded image file"""
-    image = Image.open(io.BytesIO(file_bytes))
-    text = pytesseract.image_to_string(image)
+    """Extract insurance info from uploaded image file using insurance_ocr module"""
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as tmp:
+        tmp.write(file_bytes)
+        tmp.flush()
+        result = insurance_ocr.analyze_insurance_card(tmp.name)
+    return result
 
-    # TODO: Add your regex extraction logic here
-    extracted_data = {
-        "raw_text": text,
-        "deductible": None,
-        "copay": None
-    }
-    return extracted_data
+return
